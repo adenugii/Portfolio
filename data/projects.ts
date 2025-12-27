a483dd1258eefed466eca4c4bc4705d4ee591aee
adenugroho31 <adenugroho31122004@gmail.com>
2025-12-27 18:54:45 +0700

    add favicon

--- data/projects.ts
+++
@@ -1,9 +0,0 @@
-export const projects = [
-  {
-    title: "PrediAI",
-    description: "Android app for predicting...",
-    tech: ["Kotlin", "Jetpack Compose", "Python"],
-    link: "github.com/...",
-  },
-  // Tambah project baru tinggal tambah object di sini
-];
